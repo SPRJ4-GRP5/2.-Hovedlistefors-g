39528c0f663803aea8b750ce853782cb1112b9ae
Quynh Trang Mac <201810269@post.au.dk>
2020-04-06 09:47:59 +0200

    Added HovedlisteContext and models classes + extra

---
+++ Models/Sager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hovedliste.Models
+{
+    public class Sager
+    {
+        public int Billede { get; set; }
+        public string Emne { get; set; }
+        public string Tekst { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Hovedliste.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hovedliste
 {
@@ -24,6 +26,9 @@ namespace Hovedliste
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<HovedlisteContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("HovedlisteContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
